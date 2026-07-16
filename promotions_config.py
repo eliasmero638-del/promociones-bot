3ad5ef184fb15315755e85107ea3942e055ca97e
eliasmero638-del <eliasmero638@gmail.com>
2026-07-15 23:24:24 -0500

    Replace promotions_config.py with 6 sample promotions and PROMOTION_INTERVAL

--- promotions_config.py
+++ promotions_config.py
@@ -2,9 +2,36 @@
 
 # Add your promotion configurations here
 PROMOTIONS = [
-    # Example: {
-    #     'name': 'Example Promotion',
-    #     'description': 'Description of the promotion',
-    #     'discount': '20%',
-    # }
+    {
+        'name': 'Summer Sale',
+        'description': 'Get up to 30% off on all summer items',
+        'discount': '30%',
+    },
+    {
+        'name': 'Back to School',
+        'description': 'Special discount on school supplies and clothing',
+        'discount': '25%',
+    },
+    {
+        'name': 'Flash Deal',
+        'description': 'Limited time offer on selected products',
+        'discount': '50%',
+    },
+    {
+        'name': 'Weekend Special',
+        'description': 'Exclusive weekend promotion for all members',
+        'discount': '15%',
+    },
+    {
+        'name': 'New Year Clearance',
+        'description': 'Year-end clearance sale on all categories',
+        'discount': '40%',
+    },
+    {
+        'name': 'Loyalty Rewards',
+        'description': 'Extra savings for our loyal customers',
+        'discount': '20%',
+    },
 ]
+
+PROMOTION_INTERVAL = 7200
